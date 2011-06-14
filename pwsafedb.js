d726d4ffcb49bb8432d10f2b2463df60fa49520f
Joey Hewitt <joey@joeyhewitt.com>
2011-06-13 21:43:32 -0600

    first commit

---
+++ pwsafedb.js
@@ -0,0 +1,173 @@
+function PWSafeDB(data) {
+    this._view = new jDataView(data);
+}
+$.extend(PWSafeDB.prototype, {
+BLOCK_SIZE: 16,
+validate: function() {
+    if (this._view.getString(4) != "PWS3") {
+        throw "Not a PWS v3 file";
+    }
+
+    this._eofMarkerPos = this._view.length-32-this.BLOCK_SIZE;
+    if (this._eofMarkerPos <= 0 || (this._view.getString(this.BLOCK_SIZE, this._eofMarkerPos) != "PWS3-EOFPWS3-EOF")) {
+        throw "No EOF marker found - not a valid v3 file, or it's corrupted";
+    }
+
+    return true;
+},
+decrypt: function(key) {
+    this.validate();
+
+    var salt = this._view.getString(32, 4);
+    var iter = this._view.getUint32();
+    var expectedStretchedKeyHash = this._getHexStringFromBytes(this._view, 32);
+    var stretchedKey = this._stretchKeySHA256(key, salt, iter);
+    var stretchedKeyHash = Crypto.SHA256(stretchedKey);
+
+    if (expectedStretchedKeyHash !== stretchedKeyHash) {
+        throw "Incorrect password";
+    }
+
+    var keyView = this._dataViewFromPlaintext(TwoFish.decrypt(this._view, 4, stretchedKey));
+    var K = this._getByteArray(keyView, 32);
+    var L = this._getByteArray(keyView, 32);
+
+    var numRecordBlocks = (this._eofMarkerPos-this._view.tell())/this.BLOCK_SIZE;
+    var recordView = this._dataViewFromPlaintext(TwoFish.decrypt(this._view, numRecordBlocks, K, true));
+
+    this.headers = this._parseHeaders(recordView);
+    this.records = this._parseRecords(recordView);
+
+    // check hash of plaintext fields
+    /*this._view.seek(this._eofMarkerPos+this.BLOCK_SIZE);
+    recordView.seek(this._headers.versionNumberOffset);
+    var actualHMAC = Crypto.HMAC(Crypto.SHA256,
+        this._getByteArray(recordView, recordView.length-this._headers.versionNumberOffset),
+        L, {asBytes: true});
+    var expectedHMAC = this._getByteArray(this._view, 32);
+
+    if (expectedHMAC !== actualHMAC) {
+        throw "HMAC didn't match -- something may be corrupted";
+    }*/
+
+    // sort by title
+    this.records = this.records.sort(function(a, b) {
+        return a.title.localeCompare(b.title);
+    });
+
+    // clean up raw data
+    delete this._view;
+    delete this._eofMarkerPos;
+},
+_parseHeaders: function(recordView) {
+    var headers = {};
+    var fieldType = undefined;
+    while(fieldType != 0xff) {
+        var recordBegin = recordView.tell();
+        var fieldSize = recordView.getUint32();
+        fieldType = recordView.getUint8();
+        switch(fieldType) {
+        case 0x00: // Version
+            headers.versionNumber = recordView.getUint16();
+            headers.versionNumberOffset = recordView.tell() & ~(this.BLOCK_SIZE - 1);
+            break;
+        case 0x01: // UUID
+            headers.UUID = Crypto.util.bytesToHex(this._getByteArray(recordView, 16));
+            break;
+        case 0x04: // Last saved time
+            headers.lastSaveTime = new Date(recordView.getUint32() * 1000);
+            break;
+        case 0x06: // Last save app
+            headers.lastSaveApp = recordView.getString(fieldSize);
+            break;
+        case 0x07: // Last save user
+            headers.lastSaveUser = recordView.getString(fieldSize);
+            break;
+        case 0x08: // Last save host
+            headers.lastSaveHost = recordView.getString(fieldSize);
+            break;
+        case 0x09: // Database name
+            headers.dbName = recordView.getString(fieldSize);
+            break;
+        case 0xff: // END
+            break;
+        default: // unknown or unimportant
+            recordView.seek(recordBegin + fieldSize); // skip
+        }
+        this._alignToBlockBoundary(recordView);
+    }
+    return headers;
+},
+_parseRecords: function(recordView) {
+    var currentRecord = {};
+    var records = [];
+    while (recordView.tell() < recordView.length) {
+        /*if (recordView.tell() % 16) {
+            console.log('out of alignment');
+            return false;
+        }*/
+        var fieldSize = recordView.getUint32();
+        var fieldType = recordView.getUint8();
+        var recordBegin = recordView.tell();
+        switch(fieldType) {
+        case 0x03: // Title
+            currentRecord.title = recordView.getString(fieldSize);
+            break;
+        case 0x04: // Username
+            currentRecord.username = recordView.getString(fieldSize);
+            break;
+        case 0x05: // Notes
+            currentRecord.notes = recordView.getString(fieldSize);
+            break;
+        case 0x06: // Password
+            currentRecord.password = recordView.getString(fieldSize);
+            break;
+        case 0x0d: // URL
+            currentRecord.URL = recordView.getString(fieldSize);
+            break;
+        case 0x14: // Email
+            currentRecord.email = recordView.getString(fieldSize);
+            break;
+        case 0xff:
+            records.push(currentRecord);
+            currentRecord = {};
+            break;
+        default: // unknown or unimportant
+            //console.log('unsupp type', (fieldType>>>0).toString(16));
+            //console.log('skipping '+fieldSize, recordBegin === recordView.tell());
+            recordView.seek(recordBegin + fieldSize); // skip
+        }
+        this._alignToBlockBoundary(recordView);
+    }
+
+    return records;
+},
+_dataViewFromPlaintext: function(buffer) {
+    return new jDataView(jDataView.createBuffer.apply(null, buffer));
+},
+_alignToBlockBoundary: function(view) {
+    // align to block boundary
+    var off = view.tell() % this.BLOCK_SIZE;
+    if (off) {
+        //console.log('align '+(BLOCK_SIZE - off));
+        view.seek(view.tell() + this.BLOCK_SIZE - off);
+    }
+},
+_stretchKeySHA256: function(key, salt, iter) {
+    key = Crypto.charenc.Binary.stringToBytes(key+salt);
+    for (var i = iter; i >= 0; i--) {
+        key = Crypto.SHA256(key, {asBytes: true });
+    }
+    return key;
+},
+_getHexStringFromBytes: function(view, byteCount) {
+    return Crypto.util.bytesToHex(this._getByteArray(view, byteCount));
+},
+_getByteArray: function(view, byteCount) {
+    var bytes = new Array(byteCount);
+    for(var i = 0; i < byteCount; i++) {
+        bytes[i] = view.getUint8();
+    }
+    return bytes;
+}
+});
---
+++ twofish.js
@@ -0,0 +1,231 @@
+// http://members.tele2.nl/MAvanEverdingen/Code/code.html
+(function() {
+
+var tfsKey=[];
+var tfsM=[[],[],[],[]];
+
+function tfsInit(key) {
+  var  i, a, b, c, d, meKey=[], moKey=[], inKey=[];
+  var kLen;
+  var sKey=[];
+  var  f01, f5b, fef;
+
+  var q0=[[8,1,7,13,6,15,3,2,0,11,5,9,14,12,10,4],[2,8,11,13,15,7,6,14,3,1,9,4,0,10,12,5]];
+  var q1=[[14,12,11,8,1,2,3,5,15,4,10,6,7,0,9,13],[1,14,2,11,4,12,3,7,6,13,10,5,15,9,0,8]];
+  var q2=[[11,10,5,14,6,13,9,0,12,8,15,3,2,4,7,1],[4,12,7,5,1,6,9,10,0,14,13,8,2,11,3,15]];
+  var q3=[[13,7,15,4,1,2,6,14,9,11,3,0,8,5,12,10],[11,9,5,1,12,3,13,14,6,4,7,15,2,0,8,10]];
+  var ror4=[0,8,1,9,2,10,3,11,4,12,5,13,6,14,7,15];
+  var ashx=[0,9,2,11,4,13,6,15,8,1,10,3,12,5,14,7];
+  var q=[[],[]];
+  var m=[[],[],[],[]];
+
+  function ffm5b(x){ return x^(x>>2)^[0,90,180,238][x&3]; }
+  function ffmEf(x){ return x^(x>>1)^(x>>2)^[0,238,180,90][x&3]; }
+
+  function mdsRem(p,q){
+    var i,t,u;
+    for(i=0; i<8; i++){
+      t = q>>>24;
+      q = ((q<<8)&wMax) | p>>>24;
+      p = (p<<8)&wMax;
+      u = t<<1; if (t&128){ u^=333; }
+      q ^= t^(u<<16);
+      u ^= t>>>1; if (t&1){ u^=166; }
+      q ^= u<<24 | u<<8;
+    }
+    return q;
+  }
+
+  function qp(n,x){
+    var a,b,c,d;
+    a=x>>4; b=x&15;
+    c=q0[n][a^b]; d=q1[n][ror4[b]^ashx[a]];
+    return q3[n][ror4[d]^ashx[c]]<<4 | q2[n][c^d];
+  }
+
+  function hFun(x,key){
+    var a=getB(x,0), b=getB(x,1), c=getB(x,2), d=getB(x,3);
+    switch(kLen){
+    case 4:
+      a = q[1][a]^getB(key[3],0);
+      b = q[0][b]^getB(key[3],1);
+      c = q[0][c]^getB(key[3],2);
+      d = q[1][d]^getB(key[3],3);
+    case 3:
+      a = q[1][a]^getB(key[2],0);
+      b = q[1][b]^getB(key[2],1);
+      c = q[0][c]^getB(key[2],2);
+      d = q[0][d]^getB(key[2],3);
+    case 2:
+      a = q[0][q[0][a]^getB(key[1],0)]^getB(key[0],0);
+      b = q[0][q[1][b]^getB(key[1],1)]^getB(key[0],1);
+      c = q[1][q[0][c]^getB(key[1],2)]^getB(key[0],2);
+      d = q[1][q[1][d]^getB(key[1],3)]^getB(key[0],3);
+    }
+    return m[0][a]^m[1][b]^m[2][c]^m[3][d];
+  }
+
+  key=key.slice(0,32); i=key.length;
+  while ( i!=16 && i!=24 && i!=32 ) key[i++]=0;
+
+  for (i=0; i<key.length; i+=4){ inKey[i>>2]=getW(key, i); }
+  for (i=0; i<256; i++){ q[0][i]=qp(0,i); q[1][i]=qp(1,i); }
+  for (i=0; i<256; i++){
+    f01 = q[1][i]; f5b = ffm5b(f01); fef = ffmEf(f01);
+    m[0][i] = f01 + (f5b<<8) + (fef<<16) + (fef<<24);
+    m[2][i] = f5b + (fef<<8) + (f01<<16) + (fef<<24);
+    f01 = q[0][i]; f5b = ffm5b(f01); fef = ffmEf(f01);
+    m[1][i] = fef + (fef<<8) + (f5b<<16) + (f01<<24);
+    m[3][i] = f5b + (f01<<8) + (fef<<16) + (f5b<<24);
+  }
+
+  kLen = inKey.length/2;
+  for (i=0; i<kLen; i++){
+    a = inKey[i+i];   meKey[i] = a;
+    b = inKey[i+i+1]; moKey[i] = b;
+    sKey[kLen-i-1] = mdsRem(a,b);
+  }
+
+  for (i=0; i<40; i+=2){
+    a=0x1010101*i; b=a+0x1010101;
+    a=hFun(a,meKey);
+    b=rotw(hFun(b,moKey),8);
+    tfsKey[i]=(a+b)&wMax;
+    tfsKey[i+1]=rotw(a+2*b,9);
+  }
+  for (i=0; i<256; i++){
+    a=b=c=d=i;
+    switch(kLen){
+    case 4:
+      a = q[1][a]^getB(sKey[3],0);
+      b = q[0][b]^getB(sKey[3],1);
+      c = q[0][c]^getB(sKey[3],2);
+      d = q[1][d]^getB(sKey[3],3);
+    case 3:
+      a = q[1][a]^getB(sKey[2],0);
+      b = q[1][b]^getB(sKey[2],1);
+      c = q[0][c]^getB(sKey[2],2);
+      d = q[0][d]^getB(sKey[2],3);
+    case 2:
+      tfsM[0][i] = m[0][q[0][q[0][a]^getB(sKey[1],0)]^getB(sKey[0],0)];
+      tfsM[1][i] = m[1][q[0][q[1][b]^getB(sKey[1],1)]^getB(sKey[0],1)];
+      tfsM[2][i] = m[2][q[1][q[0][c]^getB(sKey[1],2)]^getB(sKey[0],2)];
+      tfsM[3][i] = m[3][q[1][q[1][d]^getB(sKey[1],3)]^getB(sKey[0],3)];
+    }
+  }
+}
+
+function tfsG0(x){ return tfsM[0][getB(x,0)]^tfsM[1][getB(x,1)]^tfsM[2][getB(x,2)]^tfsM[3][getB(x,3)]; }
+function tfsG1(x){ return tfsM[0][getB(x,3)]^tfsM[1][getB(x,0)]^tfsM[2][getB(x,1)]^tfsM[3][getB(x,2)]; }
+
+function tfsFrnd(r,blk){
+  var a=tfsG0(blk[0]); var b=tfsG1(blk[1]);
+  blk[2] = rotw( blk[2]^(a+b+tfsKey[4*r+8])&wMax, 31 );
+  blk[3] = rotw(blk[3],1) ^ (a+2*b+tfsKey[4*r+9])&wMax;
+  a=tfsG0(blk[2]); b=tfsG1(blk[3]);
+  blk[0] = rotw( blk[0]^(a+b+tfsKey[4*r+10])&wMax, 31 );
+  blk[1] = rotw(blk[1],1) ^ (a+2*b+tfsKey[4*r+11])&wMax;
+}
+
+function tfsIrnd(i,blk){
+  var a=tfsG0(blk[0]); var b=tfsG1(blk[1]);
+  blk[2] = rotw(blk[2],1) ^ (a+b+tfsKey[4*i+10])&wMax;
+  blk[3] = rotw( blk[3]^(a+2*b+tfsKey[4*i+11])&wMax, 31 );
+  a=tfsG0(blk[2]); b=tfsG1(blk[3]);
+  blk[0] = rotw(blk[0],1) ^ (a+b+tfsKey[4*i+8])&wMax;
+  blk[1] = rotw( blk[1]^(a+2*b+tfsKey[4*i+9])&wMax, 31 );
+}
+
+function tfsClose(){
+  tfsKey=[];
+  tfsM=[[],[],[],[]];
+}
+
+function tfsEncrypt(){
+  var blk=[getW(bData)^tfsKey[0], getW(bData)^tfsKey[1], getW(bData)^tfsKey[2], getW(bData)^tfsKey[3]];
+  for (var j=0;j<8;j++){ tfsFrnd(j,blk); }
+  setW(bData,i   ,blk[2]^tfsKey[4]);
+  setW(bData,i+ 4,blk[3]^tfsKey[5]);
+  setW(bData,i+ 8,blk[0]^tfsKey[6]);
+  setW(bData,i+12,blk[1]^tfsKey[7]);
+  i+=16;
+}
+
+function tfsDecrypt(blk) {
+  blk[0] ^= tfsKey[4];
+  blk[1] ^= tfsKey[5];
+  blk[2] ^= tfsKey[6];
+  blk[3] ^= tfsKey[7];
+  for (var j=7;j>=0;j--){ tfsIrnd(j,blk); }
+  setW(outBuffer,outOffset   ,blk[2]^tfsKey[0]);
+  setW(outBuffer,outOffset+ 4,blk[3]^tfsKey[1]);
+  setW(outBuffer,outOffset+ 8,blk[0]^tfsKey[2]);
+  setW(outBuffer,outOffset+12,blk[1]^tfsKey[3]);
+}
+
+function getW(a,i){ return a[i]|a[i+1]<<8|a[i+2]<<16|a[i+3]<<24; }
+function setW(a,i,w){ a.splice(i,4,w&0xFF,(w>>>8)&0xFF,(w>>>16)&0xFF,(w>>>24)&0xFF); }
+function rotw(w,n){ return ( w<<n | w>>>(32-n) ) & wMax; }
+function getB(x,n){ return (x>>>(n*8))&0xFF; }
+wMax = 0xffffffff;
+
+window.TwoFish = {
+    decrypt: function(dataView, blockCount, key, cbcMode) {
+        var BLOCK_SIZE = 16;
+        tfsInit(key);
+
+        outBuffer = new Array(blockCount * BLOCK_SIZE);
+        outOffset = 0;
+
+        if (cbcMode) {
+            // skip initialization vector
+            dataView.seek(dataView.tell() + BLOCK_SIZE);
+            blockCount--;
+        }
+
+        while (blockCount--) {
+            //console.log('block');
+            var block = [dataView.getUint32(), dataView.getUint32(), dataView.getUint32(), dataView.getUint32()];
+
+            tfsDecrypt(block);
+            /*for (var i = outOffset; i < outOffset + BLOCK_SIZE; i++) {
+                console.log((outBuffer[i]>>>0).toString(16));
+            }*/
+
+            if (cbcMode) {
+                var tell = dataView.tell();
+                dataView.seek(tell - 2*BLOCK_SIZE);
+                for (var i = 0; i < 16; i += 4) {
+                    setW(outBuffer, outOffset + i, getW(outBuffer, outOffset + i) ^ dataView.getUint32());
+                }
+                dataView.seek(tell);
+                /*block[0] ^= getW(outBuffer, outOffset-16);
+                block[1] ^= getW(outBuffer, outOffset-12);
+                block[2] ^= getW(outBuffer, outOffset-8);
+                block[3] ^= getW(outBuffer, outOffset-4);*/
+            }
+
+            outOffset += 16;
+        }
+
+        return outBuffer;
+    }
+};
+
+})();
+/*console.log(
+TwoFish.decrypt(
+new jDataView(jDataView.createBuffer.apply(null, Crypto.util.hexToBytes("37527BE0052334B89F0CFCCAE87CFA20"))), 1,
+Crypto.util.hexToBytes("0123456789ABCDEFFEDCBA987654321000112233445566778899AABBCCDDEEFF"))
+);*/
+/*console.log(
+TwoFish.decrypt(
+new jDataView(jDataView.createBuffer.apply(null, Crypto.util.hexToBytes("00000000000000000000000000000000"))), 1,
+Crypto.util.hexToBytes("0000000000000000000000000000000000000000000000000000000000000000"))
+);*/
+/*
+console.log(
+TwoFish.decrypt(
+new jDataView(jDataView.createBuffer.apply(null, Crypto.util.hexToBytes("1B1FE8F5A911CD4C0D800EDCE8ED0A942CBA6271A1044F90C30BA8FE91E1C163"))), 1,
+Crypto.util.hexToBytes("1B1FE8F5A911CD4C0D800EDCE8ED0A942CBA6271A1044F90C30BA8FE91E1C163"))
+);*/
